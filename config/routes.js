81535d481afb314700aa0af7114fa7d9ec4946ee
Nicholas Weiler <nweiler@Nicholass-MacBook-Pro.local>
2015-09-20 20:32:23 -0400

    Lots of updates. Bootstrap kinda working.

--- config/routes.js
+++ config/routes.js
@@ -1,48 +1,10 @@
-var assert = require('assert');
-var async = require('async');
-var MongoClient = require('mongodb').MongoClient;
-var express = require('express');
-var app = express();
-var ejs = require('ejs');
-var bodyParser = require('body-parser');
-var router = express.Router();
 
-app.set('port', (process.env.PORT || 3000));
-app.set('views', __dirname + '/views')
-app.set('view engine', 'ejs')
-app.use(express.static(__dirname + '/public'));
+var users = require('./users');
+var entries = require('./entries');
 
-var api = {};
-module.exports = api;
+module.exports = function(app, passport) {
 
-var url = 'mongodb://localhost:27017/entries'
-
-MongoClient.connect(url, function(e, db) {
-  if (e) return console.log(e);
-  console.log("Connected to Mongo");
-
-  var entry = {
-    'date': '1/1/15',
-    'location': 'RJJA Michiana',
-    'instructor':' Gary Briscoe',
-    'time': '6:30 PM',
-    'moves': [],
-    'rounds': 4,
-    'notes': 'n/a'
-  };
-  
-  var del_entry = {
-    'date': '1/1/15'
-  };
-
-  app.get('/', function(req, res) {
-    api.read_all(db, function(err, data) {
-      res.render('navbar', data);
-      //console.log(data);
-      //res.send(data);
-    });
-  });
-  
+  app.get('/', users.login);
   app.get('/add', function(req, res) {
     api.create(db, entry, function(err, data) {
       api.read_all(db, function(err, data) {
